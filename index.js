461f74273a9c4059c3a6e2e682457ca11ee5950f
abusayedwd <110084857+abusayedwd@users.noreply.github.com>
2024-05-24 16:26:03 +0600

    products dataget from server

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const app = express();
 const port = process.env.PORT || 5000 ;
 
 
-app.use(cors());
+app.use(cors()); 
 app.use(express.json());
 
  
-console.log(process.env.DB_PSSSWORD)
+// console.log(process.env.DB_PSSSWORD)
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PSSSWORD}@cluster0.b1f0ncj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -27,13 +27,23 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    await client.connect(); 
+
+    const productCollections = client.db('EcommerceDB').collection('products')
+   
+    app.get('/products', async(req, res) => {
+       const cursor = productCollections.find();
+       const result = await cursor.toArray();
+       res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+   //  await client.close();
   }
 }
 run().catch(console.dir);
